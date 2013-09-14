096dccdb9e632ae05d738ae352d4a70c1971f62b
Vido <vido@usp.br>
2013-09-14 20:37:33 -0300

    Inserting data into a database.

---
+++ extractor.py
@@ -0,0 +1,81 @@
+import csv
+import sqlite3
+
+def format_query(table, rows, reals_indexes):
+    q ="INSERT INTO %s VALUES (" % table
+    for row, i in zip(rows, range(len(rows))):
+        if i == len(rows)-1:
+            break
+        
+        c_row = row.replace(',', '.') if ',' in row else row
+        c_row = c_row.replace('\n', ' ') if '\n' in c_row else c_row
+            
+        if i not in reals_indexes:
+            value = "'"+ c_row + "'"
+        else:
+            value = float(c_row)
+             
+        q += str(value) + ","
+
+    query = q[:-1] + ");"
+
+    return query
+
+conn = sqlite3.connect('db')
+c = conn.cursor()
+
+print 'data/SEGMENTOS_BRASILIA_T_DEFEITO.csv ...'
+
+with open('data/SEGMENTOS_BRASILIA_T_DEFEITO.csv', 'rb') as fp:
+    csvfp = csv.reader(fp, 'excel', delimiter = ';')
+    csvfp.next()
+
+    for line in csvfp:
+        try:
+            query = format_query("T_DEFEITOS", line, [3])
+            c.execute(query)
+        except Exception as e:
+            #print line
+            print query
+            print repr(e)
+
+    conn.commit()
+
+print 'data/SEGMENTOS_BRASILIA_T_AVALIACAO.csv ...'
+
+with open('data/SEGMENTOS_BRASILIA_T_AVALIACAO.csv', 'r') as fp:
+    csvfp = csv.reader(fp, 'excel', delimiter = ';')
+    csvfp.next()
+
+    for line in csvfp:
+        try:
+            query = format_query("T_AVALIACAO", line, [6])
+            c.execute(query)
+        except Exception as e:
+            #print line
+            print query
+            print repr(e)
+
+    conn.commit()
+
+
+print 'data/SEGMENTOS_BRASILIA_T_INVENTARIO.csv ...'
+
+with open('data/SEGMENTOS_BRASILIA_T_INVENTARIO.csv', 'r') as fp:
+    csvfp = csv.reader(fp, 'excel', delimiter = ';')
+    csvfp.next()
+
+    for line in csvfp:
+        try:
+            query = format_query("T_INVENTARIO", line, [])
+            c.execute(query)
+        except Exception as e:
+            #print line
+            print query
+            print repr(e)
+
+    conn.commit()
+
+
+conn.close()
+
---
+++ schema.py
@@ -0,0 +1,96 @@
+import sqlite3
+
+conn = sqlite3.connect('db')
+c = conn.cursor()
+
+# T_DEFEITOS
+c.execute('''
+    CREATE TABLE T_DEFEITOS(
+        RA30 text,
+        CODIGO_DEFEITO text,
+        FAIXA_ANALISADA text,
+        EXTENSAO real,
+        VERIFICAR text,
+        CODIGO_SEVERIDADE text,
+        CODIGO_TIPO_DEFEITO text,
+        PD_AVALIADOR text,
+        PD_PONDERADOS text,
+        CODIGO_AVALIACAO text,
+        FATOR_UTILIZADO text,
+        CODIGO_SEGMENTO_PISTA text
+    )'''
+)
+
+# T_AVALIACAO 
+c.execute('''
+    CREATE TABLE T_AVALIACAO(
+        A30 text,
+        CODIGO_AVALIACAO text,
+        CODIGO_SEGMENTO_PISTA text,
+        ICP_PD text,
+        ACEITABILIDADE text,
+        OBSERVACAO text,
+        ICP_AV real,
+        MES text,
+        ANO text,
+        AVALIACAO_COMPLEMENTAR text,
+        DATA_AVALIACAO text,
+        TOTAL_PD_AV text,
+        CODIGO_ICP_CT text,
+        CODIGO_MR_PREVISTA text,
+        CODIGO_FUNCIONARIO_DIGITADOR text,
+        CODIGO_FUNCIONARIO_AVALIADOR text,
+        QTDE_DEF text,
+        DATA_DIG text,
+        ID_DIGITADOR text,
+        OBS1 text,
+        ICP_SE text,
+        TOTAL_SE text,
+        PONTOS_SE text,
+        CODIGO_PARAM_CHECAGEM text,
+        CODIGO_TP_AVALIACAO text,
+        TOTAL_PD text
+    )'''
+)
+
+# T_INVENTARIO
+c.execute('''
+    CREATE TABLE t_inventario(
+        RA30 text,
+        CODIGO_INVENTARIO text,
+        CODIGO_SEGMENTO_PISTA text,
+        CODIGO_PAVIMENTO text,
+        CODIGO_VOLUME text,
+        CODIGO_ROTA text,
+        DATA_ANALISE text,
+        NOME_RUA text,
+        COMPRIMENTO text,
+        LARGURA_PISTA text,
+        IDADE text,
+        ANO_ULTIMA_MR text,
+        CODIGO_MR text,
+        CODIGO_POCOVISITA text,
+        CODIGO_INTERFERENCIA text,
+        CODIGO_CALCADA text,
+        CODIGO_DRENAGEM text,
+        CODIGO_SUBLEITO text,
+        CODIGO_ESTRUTURA text,
+        CODIGO_SENTIDO text,
+        CODIGO_GRID text,
+        DA_RUA text,
+        ATE_RUA text,
+        NUMERO_FAIXAS text,
+        RAIZ text,
+        BOCA_LOBO text,
+        CODIGO_CONDICAO_DRENAGEM text,
+        CODIGO_ESTACIONAMENTO text,
+        CODIGO_MEIOFIO text,
+        CODIGO_CONDICAO_MEIOFIO text,
+        CODIGO_CONDICAO_CALCADA text,
+        CODIGO_HIERARQUIA text,
+        VELOCIDADE_VIA text,
+        VEICULOS_PESADOS text
+    )'''
+)
+
+conn.close()
